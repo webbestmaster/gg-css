61e91bf0b18003e6fec0821c67d2a670ad56d4bb
Dmitry Turovtsov <web.best.master@gmail.com>
2022-06-11 20:25:55 +0200

    Add files via upload

---
+++ declaration.d.ts
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-default-export, init-declarations */
+
+declare module '*.svg' {
+    const content: string;
+
+    export default content;
+}
+
+declare module '*.png' {
+    const content: string;
+
+    export default content;
+}
+
+declare module '*.md' {
+    const content: string;
+
+    export default content;
+}
+
+declare module '*.txt' {
+    const content: string;
+
+    export default content;
+}
+
+declare const IS_PRODUCTION: unknown;
+
+declare const BUILD_DATE_H: unknown;
