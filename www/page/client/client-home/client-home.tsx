17cc2610d5314d44b7e5b626d37ee222afcde989
dim <web.best.master@gmail.com>
2022-06-11 21:55:11 +0200

    update

--- www/page/client/client-home/client-home.tsx
+++ www/page/client/client-home/client-home.tsx
@@ -1,3 +1,28 @@
+import {useState} from "react";
+
+// import foreign styles
+import style1 from '../../../material/187145_files/style-new.css';
+
+console.log(style1)
+
+import styleClientHome from './client-home.scss';
+
 export function ClientHome(): JSX.Element {
-    return <h1>111</h1>;
+    const [color, setColor] = useState<string>('#000000');
+
+    return (
+        <div className={styleClientHome.home}>
+            <div className={styleClientHome.home_container}>
+                <h1>inputs</h1>
+            </div>
+            <div className={styleClientHome.home_container}>
+                <h1>result</h1>
+
+                <div>
+
+                </div>
+
+            </div>
+        </div>
+    );
 }
